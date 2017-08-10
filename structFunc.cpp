1ef663f63f743ad46623b663b6142cb261315da5
Donggun Chung <chungdk1117@gmail.com>
2017-08-10 20:54:43 +0900

    structFunc.cpp file is changed

--- structFunc.cpp
+++ structFunc.cpp
@@ -32,7 +32,53 @@ int deviceNameCmp(void *p1, void *p2)
 }
 
 //==============================================//
+void reserveMemcpy(void *p1, void *p2)
+{
+	Reserve *val1 = (Reserve *)p1, *val2 = (Reserve *)p2;
 
+	strcpy(val1->deviceName, val2->deviceName);
+	val1->hour = val2->hour;
+	val1->min = val2->min;
+	val1->reStatus = val2->reStatus;
+	val1->mode = val2->mode;
+}
+void reservePrint(void *p)
+{
+	Reserve * val = (Reserve *)p;
+
+	printf("장치명 = %s\n", val->deviceName);
+	printf("시 = %d\n", val->hour);
+	printf("분 = %d\n", val->min);
+	if (val->reStatus == -1)
+		printf("ON 예약\n");
+	else
+		printf("OFF 예약\n");
+	if (!strcmp(val->deviceName, "AIRCOND")) {      //   에어컨
+		if (val->mode == 1)
+			printf("모드 : 강풍\n");
+		else if (val->mode == 2)
+			printf("모드 : 약풍\n");
+		else if (val->mode == 3)
+			printf("모드 : 미풍\n");
+		else
+			printf("모드 : 자연풍\n");
+	}
+	else if (!strcmp(val->deviceName, "LAUNDRY")) {  //   세탁기
+		if (val->mode == 1)
+			printf("모드 : 기본\n");
+		else if (val->mode == 2)
+			printf("모드 : 쾌속\n");
+		else
+			printf("모드 : 헹굼탈수\n");
+	}
+	else {                                 //   공기청정기
+		if (val->mode == 1)
+			printf("모드 : 저속\n");
+		else
+			printf("모드 : 고속\n");
+	}
+}
+//==============================================//
 void statusMemcpy(void *p1, void *p2)
 {
 	Status *val1 = (Status *)p1, *val2 = (Status *)p2;
